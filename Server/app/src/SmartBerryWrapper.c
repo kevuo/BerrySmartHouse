bd01dc35a66f55cf0b87831ec95706ec337dde3f
manu3193 <manu3193@gmail.com>
2016-11-20 20:24:34 -0600

    add last changes

--- Server/app/src/SmartBerryWrapper.c
+++ Server/app/src/SmartBerryWrapper.c
@@ -1,5 +1,5 @@
 //Created by manzumbado 
-//Last edited: September 7, 2016
+//Last edited: September 8, 2016
 
 #include <gpio.h>
 #include <json.h>
@@ -11,9 +11,9 @@ int doorGPIOArray[4]={17, 18, 19, 20};
 int garageGpio=21;
 
 
-//This is intended a an wrapper around the gpio dynamic library
-//to be used in nodejs, as a server in a tiny but powerfull raspberry pi
-//The way is designed allows to write to raspberry's gpio pins, and provide
+//This is intended as an wrapper for the gpio dynamic library
+//to be used in nodejs, as a server in the tiny but powerfull raspberry pi
+//The way its designed allows to write to raspberry's gpio pins, and provides
 //a simple interface to be called as an binary with parameters.
 int main(int argc,char *argv[]){
 	
@@ -63,6 +63,15 @@ int setPinValue( char * pinNumChar, char * valueChar){
   	return 0;
 }
 
+//Set all lights to the given state
+int setAllLights( char * valueChar){
+	int pinValue= strtol(valueChar, NULL, 10);
+	for(int i = 0; i<6; i++){
+		digitalWrite(ledGPIOArray[i], pinValue);
+	}
+  	return 0;
+}
+
 
 //Get lights state while building JSON file, the return created JSON
 int lightsState(){
@@ -99,7 +108,7 @@ int doorsState(){
 		json_object_object_add(jsonObj,"value",jPinValue);
 		json_object_array_add(jsonArray,jsonObj);
 	}
-	json_object_object_add(jsonObjTop,"lights",jsonArray);
+	json_object_object_add(jsonObjTop,"doors",jsonArray);
 	//its better to return a string rather than open and write a file
 	//FILE *fp = fopen("../data/lightsState.json", "w");
 	printf("%s", json_object_to_json_string(jsonObjTop));
@@ -114,10 +123,10 @@ int garageState(){
 	json_object * jPinNum= json_object_new_int(garageGpio);
 	json_object * jPinValue = json_object_new_int(res);
 	json_object_object_add(jsonObj,"pin",jPinNum);
-	son_object_object_add(jsonObj,"value",jPinValue);
+	json_object_object_add(jsonObj,"value",jPinValue);
 	//its better to return a string rather than open and write a file
 	//FILE *fp = fopen("../data/lightsState.json", "w");
-	printf("%s", json_object_to_json_string(jsonArray));
+	printf("%s", json_object_to_json_string(jsonObj));
 	return 0;
 }
 
@@ -133,6 +142,7 @@ void help(){
 	printf("%s\n", "You must type one of the folowing options");
 	printf("%s\n", "'-c' to configure pins");
 	printf("%s\n", "'-l' to get lights state");
+	printf("%s\n", "'-l value' to set all lights to the given state");
 	printf("%s\n", "'-d' to get doors state");
 	printf("%s\n", "'-g' to get garage door state");
 	printf("%s\n", "'-s pinNumber value' to set pin value");
@@ -146,6 +156,9 @@ if(argc>1 && argv[1][0] == '-'){
 
 		switch (argv[1][1]){
 
+			case 'a':
+							setAllLights(argv[2]);
+							break;
 			case 'c':
 							configure();
 							break;
